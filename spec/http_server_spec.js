c78c1037075c82adf8dc229eea714a9642419da7
elg0nz <elg0nz@gmail.com>
2013-05-02 01:39:19 -0700

    Fix the testsuite

--- spec/http_server_spec.js
+++ spec/http_server_spec.js
@@ -1,22 +1,32 @@
-var request = require('http');
+var http = require('http');
 
 describe('async', function () {
-    it('Server should respond to /', function (){
-        request.get('http://127.0.0.1:8124', function(response){
+    it('Server should respond to /', function (done){
+        http.get('http://127.0.0.1:8124', function(response){
             expect(response.statusCode).toBe(200);
+            done();
         });
     });
 
-    it('Can render home', function (){
-        request.get('http://127.0.0.1:8124', function(response){
-            expect(response.data).toMatch(/Raspberry/);
+    it('Can render home', function (done){
+        var body = '';
+        http.get('http://127.0.0.1:8124', function(response){
+            response.on('data', function (chunk) {
+                body += chunk;
+                expect(body).toMatch(/Raspberry/);
+                done();
+            });
         });
     });
 
-    it('Can serve static content', function(){
-        request.get('http://127.0.0.1:8124/static/js/bootstrap.min.js', function(response){
-            expect(response.statusCode).toBe(200);
-            expect(response.data).toMatch(/Bootstrap/);
+    it('Can serve static content', function(done){
+        var body = '';
+        http.get('http://127.0.0.1:8124/static/js/bootstrap.min.js', function(response){
+            response.on('data', function (chunk) {
+                body += chunk;
+                expect(body).toMatch(/Bootstrap/);
+                done();
+            });
         });
     });
 });
